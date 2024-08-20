241d363bd2c9be2de0d8825f6a1741457013df48
ZNX <henrique.baronceli@gmail.com>
2024-08-21 00:03:28 +1200

    update parameters

--- parameters.py
+++ parameters.py
@@ -11,13 +11,33 @@
 # and set the network basic configuration thereon.
 
 NETWORK_ID = 1
-MAX_SUPPLY = 1000000  # set 0 to infinite
-DECIMALS = 10  # number of decimals for the coin
-BLOCK_REWARD = 50
-APPENDIX_BLOCK_REWARD = 10
-EPOCH = 128  # amount of blocks per epoch
-HALVING_EPOCH_INTERVAL = 100  # amount of epochs between halvings, set 0 to disable halving
-BLOCK_TIME = 15  # in seconds
-TX_FEE = 1  # 10 decimals, so 1 becomes 0.0000000001 coin
-BLOCK_SIZE = 64  # maximum size in kilobytes, blocks can also be empty
-DATA_DIRECTORY = "/blockchain"  # folder where the blockchain will be stored
+MAX_SUPPLY = 1000000  # Set 0 to infinite supply
+DECIMALS = 10  # Number of decimals of the native coin
+BLOCK_REWARD = 12500000000  # Number of coins initially rewarded per block mined in decimal points (eg. If 10 decimals, 12500000000 = 1.25)
+APPENDIX_BLOCK_REWARD = 10  # Percentage of total reward paid to appendix blocks (eg. 10 = 10%)
+EPOCH = 40320  # Number of blocks per Epoch
+HALVING_EPOCH_INTERVAL = 100  # Number of Epochs between halvings, set 0 to disable halving
+BLOCK_TIME = 15  # In seconds
+RAW_TX_FEE = 1  # Flat fee charged per raw transaction in decimal points (eg. If 10 decimals, 1 = 0.0000000001)
+KB_TX_FEE = 100  # Additional fee charged per kilobyte of space used in decimal points (eg. If 10 decimals, 100 = 0.0000000100)
+BLOCK_SIZE = 256  # Maximum block size in kilobytes
+DATA_DIRECTORY = "/blockchain"  # Folder where the blockchain is stored
+SMART_CONTRACTS = True  # Toggle smart contracts on/off
+FTS = True  # Toggle fungible tokens on/off
+NFTS = True  # Toggle non-fungible tokens on/off
+
+ALLOCATIONS = [
+# Examples on how to structure pre-funding allocations to wallets:
+#    {
+#        "address": "3b6a27bccebfb8b348b87f3ed82f9220c0f86a54",  # Example address
+#        "balance": 5000000000000  # 50,000 coins (in smallest unit, considering 10 decimals)
+#    },
+#    {
+#        "address": "7f6a9dbe8d3bb8f328b67c3ed86e9200c2f86a98",  # Another example address
+#        "balance": 2500000000000  # 25,000 coins (in smallest unit, considering 10 decimals)
+#    }
+]
+
+NODE_STORAGE_FULL = 0 # Number of blocks stored by FULL nodes, set 0 to store the full blockchain history
+NODE_STORAGE_ACCESS = 40320 # Number of blocks stored by ACCESS nodes, set 0 to store the full blockchain history
+NODE_STORAGE_LIGHT = 240 # Number of blocks stored by LIGHT nodes, set 0 to store the full blockchain history
